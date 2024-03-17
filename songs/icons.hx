8ea9b858c3ef3876c9b34d2bd82b111aa5317f34
CharlesCatYT <sptubea@gmail.com>
2024-03-17 19:22:00 -0400

    Update icons.hx

--- songs/icons.hx
+++ songs/icons.hx
@@ -1,5 +1,5 @@
-public var icoP1:HealthIcon;
-public var icoP2:HealthIcon;
+static var icoP1:HealthIcon;
+static var icoP2:HealthIcon;
 
 function postCreate() {
     icoP1 = new HealthIcon(boyfriend != null ? boyfriend.getIcon() : "face", true);
@@ -12,22 +12,22 @@ function postCreate() {
 
     for (i in [iconP1, iconP2]) remove(i); // fuck you og icons
 }
-function update(elapsed:Float){
+function update(elapsed:Float) {
     icoP1.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 1, 0)) - 26);
-	icoP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 1, 0))) - (icoP2.width - 26);
+    icoP2.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 1, 0))) - (icoP2.width - 26);
 
     icoP1.health = healthBar.percent / 100;
     icoP2.health = 1 - (healthBar.percent / 100);
 }
 
-function beatHit(){
-    for (i in [icoP1, icoP2]){
+function beatHit() {
+    for (i in [icoP1, icoP2]) {
         i.scale.set(1.2, 0.85);
         FlxTween.tween(i.scale, {x: 1, y: 1}, (0.95 * (1 / (Conductor.bpm / 60))), {ease: FlxEase.expoOut});
     }
 
 icoP2.origin.set(140, 120);
 icoP1.origin.set(0, 120);
-    //icoP2.origin.set(150, 100);
+//icoP2.origin.set(150, 100);
 //icoP1.origin.set(0, 100);
 }
