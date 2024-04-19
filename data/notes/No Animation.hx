5b1f9f1b0a3f87f05a78f38a191fad9937cb9ada
CharlesCatYT <sptubea@gmail.com>
2024-04-18 21:11:02 -0400

    idk i just want to

--- data/notes/No Animation.hx
+++ data/notes/No Animation.hx
@@ -1,3 +1,3 @@
-function onNoteHit(event)
-    if (event.noteType == "No Animation")
-        event.cancelAnim();
+function onNoteHit(event) if (event.noteType == "No Animation") event.cancelAnim();
+
+function onPlayerMiss(event) if (event.noteType == "No Animation") event.cancelAnim();
