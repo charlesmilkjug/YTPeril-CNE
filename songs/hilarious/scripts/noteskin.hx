63553a395fbadd4609f106444e663f590a6231b8
CharlesCatYT <sptubea@gmail.com>
2024-04-21 16:13:57 -0400

    finally adding noteskins

---
+++ songs/hilarious/scripts/noteskin.hx
@@ -0,0 +1,10 @@
+var daStrumID:Int = 0; // strumline thats having the note skin
+
+// notes
+function onNoteCreation(e)
+    // if (e.strumLineID == daStrumID)
+        e.noteSprite = "game/notes/nutshell_notes"; // add your note skin file name here!
+// strums
+function onStrumCreation(e)
+    // if (e.player == daStrumID)
+        e.sprite = "game/notes/nutshell_notes"; // add your note skin file name here!
